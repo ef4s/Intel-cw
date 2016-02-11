4606cc0711ac473493462dead8b078fad0153922
ef4s <peter.efstathiou@gmail.com>
2016-02-11 13:57:43 +0000

    Completed Directed Network graph

--- graphs.py
+++ graphs.py
@@ -1,12 +1,21 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pylab
 
-G=nx.Graph()
+G = nx.DiGraph()
 
-spanTree = [[ 4.,  5.], [ 3.,  4.], [ 0.,  7.], [ 1.,  7.], [ 1.,  6.], [ 1.,  4.], [ 7.,  8.], [ 0.,  2.]]
+spanTree = [[0,  7], [ 0,  2], [ 1,  7], [ 7,  8], [ 1,  6], [ 1,  4], [4,  5], [4,  3]]
+spanTree = [(edge[0], edge[1]) for edge in spanTree]
+print(spanTree)
+nodes = list(range(9))
 
-for edge in spanTree:
-    G.add_edge(edge[0], edge[1])    
-    
+G.add_nodes_from(nodes)
+G.add_edges_from(spanTree)
 
-nx.draw(G)
+pos = nx.spectral_layout(G)
+#pos = nx.random_layout(G)
+
+nx.draw(G, pos, node_size=1500, node_color = ['r','r','b','b','b','b','b','b','b'], with_labels=True)#,)#, with_labels=True)
+#nx.draw_networkx_labels(G,pos, nodes)
+# plt.show()
+plt.show()
